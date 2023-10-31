123c763ea298c964b94e8d21d2b5bc178f97152c
jim <trashjim74@gmail.com>
2023-10-31 22:06:34 +0700

    linux

--- exp_w2v_own/runall.sh
+++ exp_w2v_own/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of notebooks to be executed
-notebooks=("12_TextRank_cooccur.ipynb" "13a_TextRank_w2v_lg_sim.ipynb" "13a_TextRank_w2v_sm_sim.ipynb" "13b_TextRank_w2v_lg_combined.ipynb" "13b_TextRank_w2v_sm_combined.ipynb")
+notebooks=("12_TextRank_cooccur.ipynb" "13a_TextRank_w2v_lg_sim.ipynb" "13a_TextRank_w2v_sm_sim.ipynb" "13b_TextRank_w2v_lg_combined.ipynb" "13b_TextRank_w2v_sm_combined.ipynb" "14b_TR_w2v_lg_posfilter.ipynb" "15b_TR_w2v_lg_pos_posisi.ipynb" "16b_TR_w2v_lg_pos_posisi_wfrase.ipynb")
 
 # Iterate over the list and execute each notebook
 for notebook in "${notebooks[@]}"
