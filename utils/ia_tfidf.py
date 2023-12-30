7f17401cf4ad12df3fd7f0505742824b84080ff4
jim <trashjim74@gmail.com>
2023-12-30 18:57:01 +0700

    fastext & separate Bigfile

--- utils/ia_tfidf.py
+++ utils/ia_tfidf.py
@@ -1,5 +1,4 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from scipy.sparse import coo_matrix
 
 #Function for sorting tf_idf in descending order
